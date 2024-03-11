ab3b931e96fea085dd1f27f69a7d18ebf00a650d
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-03-11 10:02:19 +0700

    Update Routes

--- routes/BarangRoute.js
+++ routes/BarangRoute.js
@@ -1,14 +1,23 @@
 import express from "express";
 import {getBarang, getBarangById, createBarang, updateBarang, deleteBarang} from "../controllers/BarangController.js"
 import { PrismaClient } from "@prisma/client";
+import multer, { diskStorage } from "multer";
 
 const prisma = new PrismaClient();
 
 const router = express.Router();
+const upload = multer({ storage: diskStorage({
+    destination: './uploads',
+    filename: (_, file, cb) => {
+      const filename = `${Date.now()}-${file.originalname}`;
+      return cb(null, filename);
+    },
+  }),
+});
 
 router.get('/barang',getBarang);
 router.get('/barang/:id_barang',getBarangById);
-router.post('/barang',createBarang);
+router.post('/barang', upload.single('gambar_barang'), createBarang);
 router.patch('/barang/:id_barang',updateBarang);
 router.delete('/barang/:id_barang',deleteBarang);
 
--- routes/DataPeminjamBarangRoute.js
+++ routes/DataPeminjamBarangRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createDataPeminjamBarang, getDataPeminjamBarang, deletetDataPeminjamBarang} from "../controllers/DataPeminjamBarangController.js"
+import {createDataPeminjamBarang, getDataPeminjamBarang, deletetDataPeminjamBarang, createKeranjang, getKeranjang} from "../controllers/DataPeminjamBarangController.js"
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -7,7 +7,9 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 router.post('/peminjamBarang',createDataPeminjamBarang);
+router.post('/keranjang',createKeranjang);
 router.get('/peminjamBarang',getDataPeminjamBarang);
+router.get('/keranjang',getKeranjang);
 router.delete('/peminjamBarang/:id_peminjam',deletetDataPeminjamBarang);
 
 
