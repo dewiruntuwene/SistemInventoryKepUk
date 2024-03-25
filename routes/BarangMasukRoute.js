3c8c627a89386cdd8391b583f4f68347799d12b5
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-03-25 22:35:01 +0700

    Update BarangMasuk with New Model

--- routes/BarangMasukRoute.js
+++ routes/BarangMasukRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getBarangMasuk, getBarangMasukById, createBarangMasuk, updateBarangMasuk, deleteBarangMasuk} from "../controllers/BarangMasukController.js"
+import {getBarangMasuk, getBarangMasukById, createBarangMasuk, updateBarangMasuk, deleteBarangMasuk, ambilBarangMasuk, buatBarangMasuk} from "../controllers/BarangMasukController.js"
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -8,6 +8,8 @@ const router = express.Router();
 
 router.get('/barangMasuk',getBarangMasuk);
 router.get('/barangMasuk/:id_barang_masuk',getBarangMasukById);
+router.get('/barangMasuk2',ambilBarangMasuk);
+router.post('/barangMasuk2',buatBarangMasuk);
 router.post('/barangMasuk',createBarangMasuk);
 router.patch('/barangMasuk/:id_barang_masuk',updateBarangMasuk);
 router.delete('/barangMasuk/:id_barang_masuk',deleteBarangMasuk);
