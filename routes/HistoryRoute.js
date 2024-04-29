7c2b06d3a621b0e24a984f6d4488772df6b4fc65
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-04-29 17:03:29 +0700

    Add updateHistory

--- routes/HistoryRoute.js
+++ routes/HistoryRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
-import {updateHistory} from "../controllers/HistoryController.js"
+import {getHistory} from "../controllers/HistoryController.js"
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const router = express.Router();
 
-router.get('/history',updateHistory);
+router.get('/history',getHistory);
 
 
 export default router;
