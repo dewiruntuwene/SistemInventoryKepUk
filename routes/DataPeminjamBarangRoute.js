275a61fb2684745073b328e2aa8c3f4ab46dce12
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-04-29 17:03:05 +0700

    Delete some stuff

--- routes/DataPeminjamBarangRoute.js
+++ routes/DataPeminjamBarangRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createDataPeminjamBarang, getDataPeminjamBarang, deletetDataPeminjamBarang, createKeranjang, getKeranjang, deleteKeranjang} from "../controllers/DataPeminjamBarangController.js"
+import {createDataPeminjamBarang, getDataPeminjamBarang, deletetDataPeminjamBarang, createKeranjang, getKeranjang, deleteKeranjang, updateKeranjang, getDataPeminjamBarangHistory} from "../controllers/DataPeminjamBarangController.js"
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -9,7 +9,9 @@ const router = express.Router();
 router.post('/peminjamBarang/',createDataPeminjamBarang);
 router.post('/keranjang',createKeranjang);
 router.get('/peminjamBarang',getDataPeminjamBarang);
+router.get('/peminjamBarangHistory',getDataPeminjamBarangHistory);
 router.get('/keranjang',getKeranjang);
+router.patch('/keranjang/:id_keranjang',updateKeranjang);
 router.delete('/peminjamBarang/:id_peminjam',deletetDataPeminjamBarang);
 router.delete('/keranjang/:id_keranjang',deleteKeranjang);
 
