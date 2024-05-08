925670d586dcc1215db1b70ac0001ea2726190f8
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-05-08 12:03:18 +0700

    Add Google Login Route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,12 +1,14 @@
 import  Express  from "express";
 const router = Express.Router();
-import { Register, Login, getUsers, Logout } from "../controllers/UserController.js";
+import { Register, Login, getUsers, Logout, getLoginGoogle, getLoginCallbackGoogle } from "../controllers/UserController.js";
 
 
 router.get('/users',getUsers);
 router.post('/login',Login);
 router.post('/register',Register);
 router.delete('/register',Logout);
+router.get('/auth/google', getLoginGoogle)
+router.get('/auth/google/callback', getLoginCallbackGoogle)
 
 
 
