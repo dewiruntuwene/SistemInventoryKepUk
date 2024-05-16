cefe3caf5caa37bdc83a529ea676e1c86e956836
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-05-16 13:01:15 +0700

    Update route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,12 +1,12 @@
 import  Express  from "express";
 const router = Express.Router();
 import { Register, Login, getUsers, Logout, getLoginGoogle, getLoginCallbackGoogle } from "../controllers/UserController.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
 
-
-router.get('/users',getUsers);
+router.get('/users',verifyToken,getUsers);
 router.post('/login',Login);
 router.post('/register',Register);
-router.delete('/register',Logout);
+router.delete('/logout',verifyToken,Logout);
 router.get('/auth/google', getLoginGoogle)
 router.get('/auth/google/callback', getLoginCallbackGoogle)
 
