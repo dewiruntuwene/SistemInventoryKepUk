8e2d215b7f902021a6c83cd5b54e7c187195ad37
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-07-02 09:53:17 +0700

    Create BarangPinjamRoute.js

---
+++ routes/BarangPinjamRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { getBarangPinjam, getBarangPinjamById, createBarangPinjam, updateBarangPinjam, deleteBarangPinjam } from "../controllers/BarangPinjamController.js";
+
+const prisma = new PrismaClient();
+
+const router = express.Router();
+
+router.get('/peminjam',getBarangPinjam);
+router.get('/peminjam/:id_transaksi_barang',getBarangPinjamById);
+router.post('/peminjam',createBarangPinjam);
+router.patch('/peminjam/:id_transaksi_barang',updateBarangPinjam);
+router.delete('/peminjam/:id_transaksi_barang',deleteBarangPinjam);
+
+export default router;
