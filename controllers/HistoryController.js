fc04ad732e722956de7d09eebe363fdd75c32ebd
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-08-04 17:02:46 +0700

    Update HistoryController.js

--- controllers/HistoryController.js
+++ controllers/HistoryController.js
@@ -21,3 +21,22 @@ export const getHistory = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const getGroupJenisBarang = async (req, res) => {
+  try {
+    // Fetch data from the database
+    const histories = await prisma.peminjam.groupBy({
+      by: ["nama_matakuliah", "nama_barang", "kode_barang"],
+      where: {
+        type: "BarangKeluar",
+      },
+      _sum: {
+        jumlah_barang: true,
+      },
+    });
+    res.json(histories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
