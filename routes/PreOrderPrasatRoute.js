f1c5ec933714a2ef5b417fabeb18ee90b83e1639
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-12-09 00:45:01 +0700

    Update PreOrderPrasatRoute.js

--- routes/PreOrderPrasatRoute.js
+++ routes/PreOrderPrasatRoute.js
@@ -3,7 +3,11 @@ import {
     createPreOrderKeranjang,
     getPreOrderKeranjang,
     createDataPreOrderPrasat,
-    getDataPreOrderPrasat
+    getDataPreOrderPrasat,
+    updateApproveKaprodiPreOrder,
+    updateRejectedKaprodiPreOrder,
+    getAllDataPreOrderPrasat,
+    updateJumlahBarangPreOrderPrasat
 } from "../controllers/PreOrderPrasatController.js";
 import { PrismaClient } from "@prisma/client";
 import multer, { diskStorage } from "multer";
@@ -17,6 +21,10 @@ router.post("/preorderkeranjang", verifyToken, createPreOrderKeranjang);
 router.get("/preorderkeranjang", verifyToken, getPreOrderKeranjang);
 router.post("/preorder", verifyToken, createDataPreOrderPrasat);
 router.get("/preorder", verifyToken, getDataPreOrderPrasat);
+router.get("/allpreorder", verifyToken, getAllDataPreOrderPrasat);
+router.patch("/preorder/:id_pre_order_paket", verifyToken, updateApproveKaprodiPreOrder);
+router.patch("/cancelPreorder/:id_pre_order_paket", verifyToken, updateRejectedKaprodiPreOrder);
+router.patch("/updateJumlahBarang/:id_preorder_detail", verifyToken, updateJumlahBarangPreOrderPrasat);
 
 
 
