e53862035f942144bb359cecce8085d4e5dc0732
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2025-01-07 20:20:55 +0700

    Update UserRoute.js

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -7,10 +7,18 @@ import {
   Logout,
   handleGoogleAuthPost,
   handleGoogleAuthGet,
+  updateUsers,
+  changePassword,
+  forgotPassword,
+  resetPassword
 } from "../controllers/UserController.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 
-router.get("/users", verifyToken, getUsers);
+router.get("/users", getUsers);
+router.put("/users/:user_id", updateUsers);
+router.patch("/changepass/", changePassword);
+router.post("/forgotpass", forgotPassword);
+router.post("/reset-password", resetPassword);
 router.post("/login", Login);
 router.post("/register", Register);
 router.delete("/logout", verifyToken, Logout);
