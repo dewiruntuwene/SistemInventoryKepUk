e775ffa64afd3b81f5047d5237589d2bc47eff22
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2025-01-09 10:10:09 +0700

    Update PrasatRoute.js

--- routes/PrasatRoute.js
+++ routes/PrasatRoute.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
   createPrasat,
   updatePrasat,
-  getDataPrasat
+  getDataPrasat,
+  deletePrasat
 } from "../controllers/PrasatController.js";
 import { PrismaClient } from "@prisma/client";
 import multer, { diskStorage } from "multer";
@@ -15,6 +16,7 @@ const router = express.Router();
 router.post("/prasat", verifyToken, createPrasat);
 router.put("/prasat/:id_prasat", updatePrasat);
 router.get("/prasat", getDataPrasat);
+router.delete("/prasat/:id_prasat", deletePrasat);
 
 
 export default router;
