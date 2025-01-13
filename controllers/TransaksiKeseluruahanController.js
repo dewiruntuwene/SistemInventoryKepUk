e2535ceb1d3b6ce882eb6801f64b9372757a1dde
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2025-01-13 23:53:45 +0700

    Update TransaksiKeseluruahanController.js

--- controllers/TransaksiKeseluruahanController.js
+++ controllers/TransaksiKeseluruahanController.js
@@ -31,6 +31,52 @@ export const getUserTransactions = async (req, res) => {
   }
 };
 
+export const getUserTransactionsAll = async (req, res) => {
+  try {
+    // Fetch Pre-Order Transactions and Group by Username
+    const response = await prisma.TransaksiOrderPreOrder.findMany({
+      select: {
+        user: {
+          select: {
+            username: true,
+          },
+        },
+        tanggal: true,
+        nama_barang: true,
+        jumlah: true,
+        nama_prasat: true,
+        jenis_transaksi: true,
+        transaksi_type: true,
+      },
+    });
+
+    // Group transactions by username
+    const groupedTransactions = response.reduce((group, transaksi) => {
+      const username = transaksi.user.username;
+
+      if (!group[username]) {
+        group[username] = [];
+      }
+      group[username].push({
+        tanggal: transaksi.tanggal,
+        nama_barang: transaksi.nama_barang,
+        jumlah: transaksi.jumlah,
+        nama_prasat: transaksi.nama_prasat,
+        jenis_transaksi: transaksi.jenis_transaksi,
+        transaksi_type: transaksi.transaksi_type,
+      });
+
+      return group;
+    }, {});
+
+    res.json(groupedTransactions);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch user transactions" });
+  }
+};
+
+
 export const getSisaBarang1 = async (req, res) => {
   const { userId } = req.params;
 
